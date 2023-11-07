d6d436a27d8841c0575e4b64ed998c3defad9ff7
zim <qwe1472571@gmail.com>
2023-11-07 11:28:40 +0800

    新增 增加viteConfig設定

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,41 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import { createHtmlPlugin } from 'vite-plugin-html';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+    const root = process.cwd();
+    const env = loadEnv(mode, root);
+
+    return {
+        base: '/',
+        plugins: [
+            react(),
+            tsconfigPaths(),
+            createHtmlPlugin({
+                minify: true,
+                entry: '/src/main.tsx',
+                inject: {
+                    data: {
+                        title: env.VITE_APP_TITLE,
+                    },
+                },
+            }),
+        ],
+        css: { preprocessorOptions: { scss: { charset: false } } },
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, 'src'),
+            },
+        },
+        server: {
+            proxy: {
+                '/api': {
+                    target: 'http://apiserver.com/',
+                    changeOrigin: true,
+                },
+            },
+        },
+    };
+});
