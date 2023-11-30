54577eb49dcab1e625b1e99ca3db16570b3027c0
zim <qwe1472571@gamil.com>
2023-11-30 14:17:20 +0800

    feat: 🎸 新增cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import 'dotenv/config';
 import { authRoute, courseRoute } from './routes/index';
 import passport from 'passport';
 import passportConfig from './config/passport';
+import cors from 'cors';
 
 const app = express();
 passportConfig();
@@ -14,6 +15,7 @@ mongoose
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/api/user', authRoute);
 
