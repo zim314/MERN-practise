4dac33b779d64891dfe1c4f0d42d4b8f723d9c14
zim <qwe1472571@gamil.com>
2023-12-01 16:27:17 +0800

    feat: 🎸 新增route基本結構

---
+++ client/src/router/pathData.tsx
@@ -0,0 +1,18 @@
+const pathData = [
+    {
+        path: '',
+        element: '',
+        children: [
+            {
+                index: true,
+                element: '',
+            },
+            {
+                path: '',
+                element: '',
+            },
+        ],
+    },
+];
+
+export default pathData;
