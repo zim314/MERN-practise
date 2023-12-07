95383d57ee08862fa8146db992cbe1f3b34eb722
zim <qwe1472571@gamil.com>
2023-12-07 17:18:09 +0800

    feat: 🎸 整理與修正course頁面

--- server/src/routes/course.ts
+++ server/src/routes/course.ts
@@ -10,8 +10,9 @@ router.use((req, res, next) => {
     next();
 });
 
+//查詢課程
 router.get('/:_id?', async (req, res) => {
-    const { _id } = req.params;
+    const _id = Object.keys(req.params).length > 0 ? req.params._id : null;
     try {
         const courseFound = await Course.find(_id ? { _id } : {})
             .populate('instructor', ['username', 'email'])
@@ -22,7 +23,53 @@ router.get('/:_id?', async (req, res) => {
     }
 });
 
-router.post('/:_id', async (req, res) => {
+//用導師ID搜尋課程
+router.get('/instructor/:_instructor_id', async (req, res) => {
+    const { _instructor_id } = req.params;
+    try {
+        const coursesFound = await Course.find({
+            instructor: _instructor_id,
+        })
+            .populate('instructor', ['username', 'email'])
+            .exec();
+        res.send({ message: '查詢成功', coursesFound });
+    } catch (error) {
+        res.status(500).send(JSON.stringify({ message: error }));
+    }
+});
+
+//用學生ID搜尋課程
+router.get('/student/:_student_id', async (req, res) => {
+    const { _student_id } = req.params;
+    try {
+        const coursesFound = await Course.find({
+            student: _student_id,
+        })
+            .populate('instructor', ['username', 'email'])
+            .exec();
+        res.send({ message: '查詢成功', coursesFound });
+    } catch (error) {
+        res.status(500).send(JSON.stringify({ message: error }));
+    }
+});
+
+//用關鍵字查詢課程
+router.post('/', async (req, res) => {
+    const { keyword } = req.body;
+    try {
+        const courseFound = await Course.find({
+            title: { $regex: keyword, $options: 'i' },
+        })
+            .populate('instructor', ['username', 'email'])
+            .exec();
+        res.send({ message: '查詢成功', courseFound });
+    } catch (error) {
+        res.status(500).send(JSON.stringify({ mssage: error }));
+    }
+});
+
+//更新課程？
+router.put('/:_id', async (req, res) => {
     const { error } = courseValidation(req.body);
     if (error)
         return res
@@ -57,54 +104,7 @@ router.post('/:_id', async (req, res) => {
     }
 });
 
-router.delete('/:_id', async (req, res) => {
-    const { _id } = req.params;
-    try {
-        const courseFound = await Course.findOne({ _id });
-        if (!courseFound)
-            return res
-                .status(400)
-                .send({ message: '並未找到課程，請重新輸入' });
-
-        if (courseFound.instructor!.equals((req.user as User)._id)) {
-            await Course.deleteOne({ _id }).exec();
-            res.send({ message: '課程刪除成功' });
-        } else {
-            res.status(403).send({ message: '只有課程擁有者才能刪除課程' });
-        }
-    } catch (error) {
-        res.status(500).send(JSON.stringify({ message: error }));
-    }
-});
-
-router.get('/instructor/:_instructor_id', async (req, res) => {
-    const { _instructor_id } = req.params;
-    try {
-        const coursesFound = await Course.find({
-            instructor: _instructor_id,
-        })
-            .populate('instructor', ['username', 'email'])
-            .exec();
-        res.send({ message: '查詢成功', coursesFound });
-    } catch (error) {
-        res.status(500).send(JSON.stringify({ message: error }));
-    }
-});
-
-router.get('/student/:_student_id', async (req, res) => {
-    const { _student_id } = req.params;
-    try {
-        const coursesFound = await Course.find({
-            student: _student_id,
-        })
-            .populate('instructor', ['username', 'email'])
-            .exec();
-        res.send({ message: '查詢成功', coursesFound });
-    } catch (error) {
-        res.status(500).send(JSON.stringify({ message: error }));
-    }
-});
-
+//註冊課程
 router.post('/', async (req, res) => {
     const { error } = courseValidation(req.body);
     if (error)
@@ -130,6 +130,27 @@ router.post('/', async (req, res) => {
         });
     } catch {
         res.status(500).send({ message: '課程註冊失敗' });
+    }
+});
+
+//刪除課程
+router.delete('/:_id', async (req, res) => {
+    const { _id } = req.params;
+    try {
+        const courseFound = await Course.findOne({ _id });
+        if (!courseFound)
+            return res
+                .status(400)
+                .send({ message: '並未找到課程，請重新輸入' });
+
+        if (courseFound.instructor!.equals((req.user as User)._id)) {
+            await Course.deleteOne({ _id }).exec();
+            res.send({ message: '課程刪除成功' });
+        } else {
+            res.status(403).send({ message: '只有課程擁有者才能刪除課程' });
+        }
+    } catch (error) {
+        res.status(500).send(JSON.stringify({ message: error }));
     }
 });
 
